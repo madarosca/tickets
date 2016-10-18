b91cf29624324afd0950b254c516f72837030736
Madalina <mada.rosca@yahoo.com>
2016-10-18 14:01:01 +0300

    tickets_v1
    tickets v1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+@section('title', 'Tickets Home')
+
+@section('content')
+
+    <div class="container">
+        <div class="row banner">
+            <div class="col-md-12">
+                <h1 class="text-center margin-top-100 editContent">
+                    Learning Laravel 5 - Owned by Mada
+                </h1>
+                <h3 class="text-center margin-top-100 editContent">Ticket Managment Application</h3>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', 'HomeController@home');
+Route::get('/create', 'TicketsController@create');
+Route::post('/create', 'TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit','TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+Route::post('/comment', 'CommentsController@newComment');
+
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('yourEmail@domain.com', 'Learning Laravel');
+
+        $message->to('yourEmail@domain.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
+
+
+
+
